d3775e2d0201c9327693d40298eaf31aeaa3c554
WonJW <85880605+WonJW@users.noreply.github.com>
2021-07-10 20:24:35 +0900

    create to do list

---
+++ js/to-do-list.js
@@ -0,0 +1,52 @@
+const toDoListSubmit = document.querySelector(".to-do-list")
+const toDoListInput = document.querySelector(".to-do-list__input");
+const toDoListLi = document.querySelector(".to-do-list__li");
+
+let toDos = [];
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter(todo => todo.id !== parseInt(li.id))
+    saveToDos()
+}
+
+function paintToDo(newToDo) {
+    const li = document.createElement("li");
+    li.id = newToDo.id;
+    const span = document.createElement("span");
+    span.innerText = newToDo.text;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo)
+    li.appendChild(button);
+    li.appendChild(span);
+    toDoListLi.appendChild(li);
+}
+
+function addToDo(event) {
+    event.preventDefault();
+    const newToDo = toDoListInput.value;
+    toDoListInput.value = "";
+    const newToDoObject = {
+        text: newToDo,
+        id: Date.now()
+    }
+    toDos.push(newToDoObject);
+    paintToDo(newToDoObject);
+    saveToDos();
+}
+
+toDoListSubmit.addEventListener("submit", addToDo)
+
+const savedToDos = localStorage.getItem("todos")
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
