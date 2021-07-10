858f7bdee5e5b8013c8147f3a80e130b6f4352dd
WonJW <85880605+WonJW@users.noreply.github.com>
2021-07-10 19:56:09 +0900

    create login

---
+++ js/login.js
@@ -0,0 +1,28 @@
+const login = document.querySelector(".login")
+const loginInput = document.querySelector(".login__write")
+const usernameText = document.querySelector(".username-text")
+
+function loginSubmit(event) {
+    event.preventDefault();
+    login.classList.add("hidden");
+    const username = loginInput.value;
+    localStorage.setItem("username", username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    usernameText.innerText = `Hello ${username}`;
+    usernameText.classList.remove("hidden");
+}
+
+const savedUsername = localStorage.getItem("username");
+
+if(savedUsername === null){
+    login.classList.remove("hidden");
+    login.addEventListener("submit", loginSubmit) ;
+} else {
+    paintGreetings(savedUsername);
+}
+
+
+
