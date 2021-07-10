fe0b21892360c0a6b8466845f5f9a009658a7de5
WonJW <85880605+WonJW@users.noreply.github.com>
2021-07-11 01:37:49 +0900

    joined quote to background
    quote => slogan

--- js/backgroud.js
+++ js/backgroud.js
@@ -11,8 +11,51 @@ const backgroundImage = [
     "real.jpg"
 ];
 
+const slogans = [
+    {
+        slogan: "Glory Glory Manchester United!",
+        teamName: "Manchester United F.C.",
+    },
+    {
+        slogan: "City Till I Die!",
+        teamName: "Manchester City F.C.",
+    },
+    {
+        slogan: "Forza Milan! Forza Italia!",
+        teamName: "A.C. Milan"
+    },
+    {
+        slogan: "Visca el Barca!",
+        teamName: "F.C. Barcelona",
+    },
+    {
+        slogan: "Keep The Blue Flag Flying High!",
+        teamName: "Chelsea F.C.",
+    },
+    {
+        slogan: "Leicester' Til I Die!",
+        teamName: "Leicester City F.C.",
+    },
+    {
+        slogan: "You'll Never Walk Alone!",
+        teamName: "Liverpool F.C.",
+    },
+    {
+        slogan: "Mia San Mia!",
+        teamName: "FC Bayern München",
+    },
+    {
+        slogan: "Allez Paris, Paris est magique!",
+        teamName: "Paris Saint-Germain F.C.",
+    },
+    {
+        slogan: "Hala Madrid!",
+        teamName: "Real Madrid C.F.",
+    }
+]
 
-const randomImage = backgroundImage[Math.floor(Math.random() * backgroundImage.length)];
+const randomNum = Math.floor(Math.random() * backgroundImage.length)
+const randomImage = backgroundImage[randomNum];
 
 const bgImage = document.createElement("img");
 
@@ -20,3 +63,11 @@ bgImage.src = `img/${randomImage}`;
 bgImage.classList.add("background-image")
 
 document.body.appendChild(bgImage);
+
+
+const slogan = document.querySelector(".team-slogan__text");
+const teamName = document.querySelector(".team-slogan__team-name");
+const randomSlogan = slogans[randomNum];
+
+slogan.innerText = randomSlogan.slogan
+teamName.innerText = `- ${randomSlogan.teamName}`
